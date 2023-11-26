aa2fcf2281de2f6f5e552bd8cccd00cfee25b4ea
Pedro Cruz <pedrovcruzcontato@gmail.com>
2023-11-26 20:04:55 -0300

    barra de pesquisa e botao para comprar livro

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: BookListScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -25,30 +27,70 @@ class BookListScreen extends StatefulWidget {
 
 class _BookListScreenState extends State<BookListScreen> {
   final String apiKey = 'AIzaSyDDKT2Yn7UkAk5rSIam95ZRxix6BI2aFOU';
-  final String googleBooksApiUrl = 'https://www.googleapis.com/books/v1/volumes';
-  final String query = 'inauthor:Stephen King';
+  final String googleBooksApiUrl =
+      'https://www.googleapis.com/books/v1/volumes';
 
-  Future<List<Book>> fetchBooks() async {
-    final response = await http.get(Uri.parse('$googleBooksApiUrl?q=$query&key=$apiKey'));
+
+  final TextEditingController _searchController = TextEditingController();
+  List<Book> _books = [];
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _searchBooks();
+  }
+
+  Future<void> _searchBooks() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final List<Book> books = await fetchBooks(_searchController.text);
+      setState(() {
+        _books = books;
+      });
+    } catch (e) {
+      // Handle error
+      print('Error: $e');
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  Future<List<Book>> fetchBooks(String query) async {
+    final response =
+        await http.get(Uri.parse('$googleBooksApiUrl?q=$query&key=$apiKey'));
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> data = json.decode(response.body);
       final List<dynamic> items = data['items'];
 
       List<Book> books = items.map((item) {
-        final volumeInfo = item['volumeInfo'];
+  final volumeInfo = item['volumeInfo'];
 
-        // Verifica se 'imageLinks' e 'thumbnail' estão presentes e não são nulos
-        final imageLinks = volumeInfo['imageLinks'];
-        final thumbnail = imageLinks != null ? imageLinks['thumbnail'] : null;
+  final imageLinks = volumeInfo['imageLinks'];
+  final thumbnail = imageLinks != null ? imageLinks['thumbnail'] : null;
 
-        return Book(
-          title: volumeInfo['title'],
-          author: volumeInfo['authors'] != null ? volumeInfo['authors'][0] : 'Autor Desconhecido',
-          description: volumeInfo['description'] ?? 'Sem descrição',
-          imageUrl: thumbnail ?? '',
-        );
-      }).toList();
+  final saleInfo = item['saleInfo'];
+  final buyLink = saleInfo != null ? saleInfo['buyLink'] : null;
+
+  return Book(
+    title: volumeInfo['title'],
+    author: volumeInfo['authors'] != null ? volumeInfo['authors'][0] : 'Autor Desconhecido',
+    description: volumeInfo['description'] ?? 'Sem descrição',
+    imageUrl: thumbnail ?? '',
+    publishedDate: volumeInfo['publishedDate'] ?? 'Data Desconhecida',
+    pageCount: volumeInfo['pageCount'] ?? 0,
+    publisher: volumeInfo['publisher'] ?? 'Editora Desconhecida',
+    categories: volumeInfo['categories'] != null ? List<String>.from(volumeInfo['categories']) : [],
+    buyLink: buyLink ?? '',
+  );
+}).toList();
+
 
       return books;
     } else {
@@ -60,42 +102,55 @@ class _BookListScreenState extends State<BookListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Livros de Stephen King'),
+        title: Text('Livros'),
       ),
-      body: FutureBuilder(
-        future: fetchBooks(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Erro: ${snapshot.error}'));
-          } else {
-            List<Book> books = snapshot.data as List<Book>;
-
-            return ListView.builder(
-              itemCount: books.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(books[index].title),
-                  subtitle: Text(books[index].author),
-                  leading: CachedNetworkImage(
-                    imageUrl: books[index].imageUrl,
-                    placeholder: (context, url) => CircularProgressIndicator(),
-                    errorWidget: (context, url, error) => Icon(Icons.error),
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => BookDetailScreen(book: books[index]),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                labelText: 'Buscar Livros',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: _searchBooks,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: _isLoading
+                ? Center(child: CircularProgressIndicator())
+                : _books.isEmpty
+                    ? Center(child: Text('Nenhum livro encontrado'))
+                    : ListView.builder(
+                        itemCount: _books.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(_books[index].title),
+                            subtitle: Text(_books[index].author),
+                            leading: CachedNetworkImage(
+                              imageUrl: _books[index].imageUrl,
+                              placeholder: (context, url) =>
+                                  CircularProgressIndicator(),
+                              errorWidget: (context, url, error) =>
+                                  Icon(Icons.error),
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      BookDetailScreen(book: _books[index]),
+                                ),
+                              );
+                            },
+                          );
+                        },
                       ),
-                    );
-                  },
-                );
-              },
-            );
-          }
-        },
+          ),
+        ],
       ),
     );
   }
@@ -120,19 +175,59 @@ class BookDetailScreen extends StatelessWidget {
             Text('Autor: ${book.author}'),
             SizedBox(height: 8),
             Text('Descrição: ${book.description}'),
+            SizedBox(height: 8),
+            Text('Data de Publicação: ${book.publishedDate}'),
+            SizedBox(height: 8),
+            Text('Número de Páginas: ${book.pageCount}'),
+            SizedBox(height: 8),
+            Text('Editora: ${book.publisher}'),
+            SizedBox(height: 8),
+            Text('Categorias: ${book.categories.join(', ')}'),
+            SizedBox(height: 16),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                _launchURL(book.buyLink);
+              },
+              child: Text('Comprar Agora'),
+            ),
             // Adicione mais detalhes conforme necessário
           ],
         ),
       ),
     );
   }
+
+_launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Não foi possível abrir o link: $url';
+    }
+  }
 }
+
 
 class Book {
   final String title;
   final String author;
   final String description;
   final String imageUrl;
+  final String publishedDate; // Adicionando a data de publicação
+  final int pageCount; // Adicionando o número de páginas
+  final String publisher; // Adicionando a editora
+  final List<String> categories; // Adicionando categorias do livro
+  final String buyLink;
 
-  Book({required this.title, required this.author, required this.description, required this.imageUrl});
+  Book({
+    required this.title,
+    required this.author,
+    required this.description,
+    required this.imageUrl,
+    required this.publishedDate,
+    required this.pageCount,
+    required this.publisher,
+    required this.categories,
+    required this.buyLink,
+  });
 }
